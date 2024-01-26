1e61e7bdafd793d8bdb7288e8ce1954d475932f7
Sujit Thapa <megasuziet@gmail.com>
2024-01-26 11:37:27 +0100

    Updated provider.tf

--- mynetwork.tf
+++ mynetwork.tf
@@ -1,14 +1,16 @@
 # Create the mynetwork network
 resource "google_compute_network" "mynetwork" {
   name = "mynetwork"
-  # RESOURCE properties go here
+  # Specify RESOURCE properties here
   auto_create_subnetworks = "true"
 }
-# Add a firewall rule to allow HTTP, SSH, RDP and ICMP traffic on mynetwork
+
+# Add a firewall rule to allow HTTP, SSH, RDP, and ICMP traffic on mynetwork
 resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
   name = "mynetwork-allow-http-ssh-rdp-icmp"
-  # RESOURCE properties go here
+  # Specify RESOURCE properties here
   network = google_compute_network.mynetwork.self_link
+  
   allow {
     protocol = "tcp"
     ports    = ["22", "80", "3389"]
@@ -16,8 +18,10 @@ resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
   allow {
     protocol = "icmp"
   }
+
   source_ranges = ["0.0.0.0/0"]
 }
+
 # Create the mynet-us-vm instance
 module "mynet-us-vm" {
   source           = "./instance"
@@ -25,7 +29,8 @@ module "mynet-us-vm" {
   instance_zone    = "us-east1-d"
   instance_network = google_compute_network.mynetwork.self_link
 }
-# Create the mynet-eu-vm" instance
+
+# Create the mynet-eu-vm instance
 module "mynet-eu-vm" {
   source           = "./instance"
   instance_name    = "mynet-eu-vm"
--- provider.tf
+++ provider.tf
@@ -1,1 +1,5 @@
-provider "google" {}
+# Specify the Google Cloud provider
+provider "google" {
+  # No additional configuration for the provider in this file
+  # You might add credentials or other configurations here if needed
+}
