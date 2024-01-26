1d87097e796273ac789b91e817a62e1493742cce
Sujit Thapa <megasuziet@gmail.com>
2024-01-26 11:39:39 +0100

    Updated variables.tf

--- instance/variables.tf
+++ instance/variables.tf
@@ -1,6 +1,13 @@
-variable "instance_name" {}
-variable "instance_zone" {}
+# Define variables for the instance name, zone, type, and network
+variable "instance_name" {
+}
+
+variable "instance_zone" {
+}
+
 variable "instance_type" {
   default = "e2-micro"
-  }
-variable "instance_network" {}
+}
+
+variable "instance_network" {
+}
