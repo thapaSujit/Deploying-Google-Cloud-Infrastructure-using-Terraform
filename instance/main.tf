49805f193fb23dda9a2a1c82fde79783bc9a2803
Sujit Thapa <megasuziet@gmail.com>
2024-01-26 11:38:29 +0100

    Updated main.tf

--- instance/main.tf
+++ instance/main.tf
@@ -1,16 +1,24 @@
+# Define the Google Compute Engine instance
 resource "google_compute_instance" "vm_instance" {
   name         = "${var.instance_name}"
   zone         = "${var.instance_zone}"
   machine_type = "${var.instance_type}"
+
+  # Configure the boot disk
   boot_disk {
     initialize_params {
+      # Specify the image for the boot disk
       image = "debian-cloud/debian-11"
-      }
+    }
   }
+
+  # Configure the network interface
   network_interface {
     network = "${var.instance_network}"
+
+    # Allocate a one-to-one NAT IP to the instance
     access_config {
-      # Allocate a one-to-one NAT IP to the instance
+      # You can add specific configurations for access if needed
     }
   }
 }
